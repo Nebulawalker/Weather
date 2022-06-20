c1c87912c9a7804e5443547f3f396b2ef8dca45f
Roman Okatev <nebulawalker@live.ru>
2022-06-21 01:00:40 +0300

    1. Обернул код в ifmain / def main. 2. Убрал комментарий, изменил название переменной payload на weather_format 3. Избавился от обертки над requests 4. Убрал лишние функции 5. Избавился от сложения строк, теперь используются f-строки.

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 import requests
 
 
-weather_service = "https://wttr.in/"
-weather_service_mirror = "http://wttr.dvmn.org/"
-payload = "nTqm&lang=ru"   #"""Параметры сервиса можно посмотреть по ссылке https://wttr.in/:help"""
-locations = ("Лондон", "Шереметьево", "Череповец")
+def main():
+    weather_service = "https://wttr.in/"
+    weather_format = "nTqm&lang=ru"
+    locations = ("Лондон", "Шереметьево", "Череповец")
 
-def get_url(service: str, location: str) -> str:
-    return service + location
-
-
-def get_response(url: str, payload: str) -> str:
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response.text
-    
-
-for location in locations:
-    try:
-       print(get_response(get_url(weather_service, location), payload))
-    except requests.exceptions.HTTPError as error:
-        print(f"Can't get data from service:\n{error}\nLet's try another one...")
+    for location in locations:
         try:
-            print(get_response(get_url(weather_service_mirror, location), payload))
+            url = f"{weather_service}{location}"
+            response = requests.get(url, params=weather_format)
+            response.raise_for_status()
+            print(response.text)
         except requests.exceptions.HTTPError as error:
-            exit(f"Can't get data from mirror serice:\n{error}")
+            print(f"Can't get data from service:\n{error}")
 
+if __name__ == '__main__':
+    main()
