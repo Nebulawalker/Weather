ec736caeacb3f51b65a60ee1f3618207da4f0b15
Roman Okatev <nebulawalker@live.ru>
2022-06-22 11:26:01 +0300

    1. Параметры GET запроса перенес в словарь. 2. Поправил название переменной wheather_format на payload

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import requests
 
 def main():
     weather_service = "https://wttr.in/"
-    weather_format = "nTqm&lang=ru"
+    payload = {"lang": "ru", "nTqm": ""}
     locations = ("Лондон", "Шереметьево", "Череповец")
 
     for location in locations:
         try:
             url = f"{weather_service}{location}"
-            response = requests.get(url, params=weather_format)
+            response = requests.get(url, params=payload)
             response.raise_for_status()
             print(response.text)
         except requests.exceptions.HTTPError as error:
